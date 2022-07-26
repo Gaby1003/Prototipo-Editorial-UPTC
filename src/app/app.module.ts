a9557c8045f013ac0b34388df9e8d642be7401a0
JeissonRuizUptc <90711266+JeissonRuizUptc@users.noreply.github.com>
2022-07-26 09:18:26 -0500

    [ADD] Carousel Component
    Adding structure carousel in body component (version initial).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CopyrightComponent } from './components/copyright/copyright.component';
+import { CarouselComponent } from './components/carousel-news/carousel.component';
 import { InformacionUptcComponent } from './components/informacion-uptc/informacion-uptc.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    CarouselComponent,
     CopyrightComponent,
     InformacionUptcComponent
   ],
@@ -22,3 +26,4 @@ import { InformacionUptcComponent } from './components/informacion-uptc/informac
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
